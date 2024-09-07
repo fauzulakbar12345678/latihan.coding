ab65c961676baea863c726f87c393ab02a42e48e
fauzulakbar12345678 <fauzulakbar456@gmail.com>
2024-09-07 11:00:52 +0700

    Add files via upload

---
+++ Pelatihan/day1.py
@@ -0,0 +1,39 @@
+# a = "apel" # "a"adlah variabel dan "apel"adalah data
+# print(a)
+
+# buah = "jeruk" #ini adalah tipedata 'string'
+# harga = 15000 #ini adalah tipedata 'integer'
+# berat = 1.5 #ini adalah tipedata 'float'
+# beli = True #ini adalah tipedata 'boolean'
+
+# print(buah)
+# print(harga)
+# print(berat)
+# print(beli)
+
+# format string
+# nama = 'siti'
+# umur = 16
+# alamat = 'Jl. buntu'
+# tinggi = 1.75
+
+# print(f"Nama saya {nama}.Saya berusia {umur} tahun,dan tinggi saya {tinggi} meter. Saya tinggal di {alamat}.")
+
+# input
+nama = input("siapa nama kamu?")
+print(f"Halo, {nama}")
+
+umur = int(input("kamu umur berapa?"))
+print(f"{umur} tahun")
+
+print("Halo, perkenalkan nama aku Bot!") 
+print("Halo, Bot!")
+nama = input("Nama, kamu siapa?")
+print(f"Namaku {nama}. Salam kenal")
+print(f"kamu sedang sibuk sekarang, {nama}")
+kegiatan = input("Kesibukanmu apa?")
+print(f"Aku sedang sibuk {kegiatan}")
+print(f"Ok, semoga {kegiatan} berhasil")
+
+            
+                
